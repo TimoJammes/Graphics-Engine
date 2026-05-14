c4108da638dba721195554b783e6eef3781b4eb8
Tim James Predator <timmyjamesthings@gmail.com>
2026-05-15 01:52:45 +0200

    redraw screen based on behavior min. movement

--- core/src/main/java/com/engine/Behavior.java
+++ core/src/main/java/com/engine/Behavior.java
@@ -2,46 +2,72 @@ package com.engine;
 
 public interface Behavior {
 
-    public void update(Entity entity, float dt);
+    public boolean update(Entity entity, float dt);
 }
 
 
 class RotateBehavior implements Behavior {
 
-    double speed; // rad/s
-    Vector3 axis;
+    private static final double DRAW_THRESHOLD = Math.PI / 180;
 
-    public RotateBehavior(double speed, Vector3 axis) {
+    double omega; // rad/s
+    float[] axis;
+    double rotationSinceLastDraw;
+
+    public RotateBehavior(double omega, float[] axis) {
         this.axis = axis;
-        this.speed = speed;
+        this.omega = omega;
     }
 
     @Override
-    public void update(Entity entity, float dt) {
-        entity.rotateWorld(speed*dt, axis.getX(), axis.getY(), axis.getZ());
+    public boolean update(Entity entity, float dt) {
+        double theta = omega * dt;
+        entity.rotateWorld(theta, Matrix.getX(axis), Matrix.getY(axis), Matrix.getZ(axis));
+
+        rotationSinceLastDraw += theta;
+        if (rotationSinceLastDraw > DRAW_THRESHOLD) {
+            rotationSinceLastDraw = 0;
+            return true;
+        }
+        return false;
     }
 }
 
 class OscillateBehavior implements Behavior {
-    float speed;
-    Vector3 axis;
-    int dir = 1;
-    Vector3 center;
 
-    public OscillateBehavior(float speed, Vector3 axis, Vector3 center) {
+    private static final float DRAW_THRESHOLD_SQR = 0.005f*0.005f;
+    float speed;
+    float[] axis;
+    int dir = 1;
+    float[] center;
+
+    float distSqrSinceLastDraw;
+
+    public OscillateBehavior(float speed, float[] axis, float[] center) {
         this.speed = speed;
         this.axis = axis;
         this.center = center;
     }
 
     @Override
-    public void update(Entity entity, float dt) {
-        float dx = speed * dt * axis.getX() * dir;
-        float dy = speed * dt * axis.getY() * dir;
-        float dz = speed * dt * axis.getZ() * dir;
+    public boolean update(Entity entity, float dt) {
+        float dx = speed * dt * Matrix.getX(axis) * dir;
+        float dy = speed * dt * Matrix.getY(axis) * dir;
+        float dz = speed * dt * Matrix.getZ(axis) * dir;
         entity.translateWorld(dx, dy, dz);
 
-        if (Matrix.normSqr(entity.transform.position.slice(0, 3).sub(center)) >= 1)
+        if (Matrix.normSqr(Matrix.sub(Matrix.getSlice(entity.transform.position, 0, 3), center)) >= 1)
             dir *= -1;
+
+//        return true;
+        distSqrSinceLastDraw += dx*dx+dy*dy+dz*dz;
+//        System.out.println(distSqrSinceLastDraw/(DRAW_THRESHOLD*DRAW_THRESHOLD));
+
+        if (distSqrSinceLastDraw > DRAW_THRESHOLD_SQR) {
+            distSqrSinceLastDraw = 0;
+            return true;
+        }
+        return false;
+
     }
 }
