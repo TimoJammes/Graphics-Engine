74407dac24acffd4f02a4da47c843f1ade972a93
Timo Jammes <timothe.jammes@gmail.com>
2026-05-25 11:46:35 +0200

    small edits + SphereEntity

--- core/src/main/java/com/engine/Entity.java
+++ core/src/main/java/com/engine/Entity.java
@@ -12,6 +12,7 @@ public class Entity implements Movable {
     final Transform transform;
     final Mesh mesh;
     Color color;
+    boolean hasNormals = false;
 
     List<Behavior> behaviors = new ArrayList<>();
 
@@ -37,10 +38,13 @@ public class Entity implements Movable {
             throw new RuntimeException(e);
         }
 
-        Mesh mesh1 = new Mesh(res.vertices, res.indices);
-//        Mesh mesh1 = new Mesh(res.vertices, res.normals, res.indices);
 
+//        Mesh mesh1 = new Mesh(res.vertices, res.indices);
+        assert res.hasNormals() : "entity loaded from .obj has no normals";
+        Mesh mesh1 = new Mesh(res.vertices, res.normals, res.indices);
+        System.out.println(res.vertices.length);
         this(mesh1, res.color);
+        hasNormals = true;
     }
 
     public void setPos(float x, float y, float z) {
@@ -49,10 +53,14 @@ public class Entity implements Movable {
         transform.position[2] = z;
     }
 
-    public void setScale(float x, float y, float z) {
-        transform.scale[0] = x;
-        transform.scale[1] = y;
-        transform.scale[2] = z;
+    public void setPos(float[] pos) {
+        setPos(pos[0], pos[1], pos[2]);
+    }
+
+    public void setScale(float s) {
+        transform.scale[0] = s;
+        transform.scale[1] = s;
+        transform.scale[2] = s;
     }
 
     public void rotateLocal(double theta, float ax, float ay, float az) {
@@ -71,22 +79,73 @@ public class Entity implements Movable {
         transform.translateLocal(dx, dy, dz);
     }
 
-    public double angleAroundAxis(float ax,  float ay, float az) {
+    public double angleAroundAxis(float ax, float ay, float az) {
         return transform.rotation.angleAroundAxis(ax, ay, az);
+    }
+}
+
+//[np.array([Rx(-np.pi/5) @ Ry(t2) @ ((Rx(t1) @ np.array([0, 0, innerRadius]) + np.array([0, 0, outerRadius])))
+// for t2 in np.linspace(0, 2*np.pi, nSidesHoriz)])
+// for t1 in np.linspace(0, 2*np.pi, nSidesVert)]
+class SphereEntity extends Entity {
+
+    SphereEntity(float radius) {
+
+        int stacks = 16;  // latitude divisions
+        int slices = 16;  // longitude divisions
+
+        int vertexCount = (stacks + 1) * (slices + 1);
+        float[] vertices = new float[vertexCount * 3];
+        int vIdx = 0;
+
+        for (int i = 0; i <= stacks; i++) {
+            float phi = (float) (Math.PI * i / stacks);        // 0 to PI
+            for (int j = 0; j <= slices; j++) {
+                float theta = (float) (2 * Math.PI * j / slices); // 0 to 2PI
+                vertices[vIdx++] = radius * (float) (Math.sin(phi) * Math.cos(theta));
+                vertices[vIdx++] = radius * (float) (Math.cos(phi));
+                vertices[vIdx++] = radius * (float) (Math.sin(phi) * Math.sin(theta));
+            }
+        }
+
+        int triangleCount = stacks * slices * 2;
+        int[] indices = new int[triangleCount * 3];
+        int iIdx = 0;
+
+        for (int i = 0; i < stacks; i++) {
+            for (int j = 0; j < slices; j++) {
+                int tl = i * (slices + 1) + j;
+                int tr = tl + 1;
+                int bl = tl + (slices + 1);
+                int br = bl + 1;
+
+                indices[iIdx++] = tl;
+                indices[iIdx++] = bl;
+                indices[iIdx++] = tr;
+
+                indices[iIdx++] = tr;
+                indices[iIdx++] = bl;
+                indices[iIdx++] = br;
+            }
+        }
+
+        Mesh mesh = new Mesh(vertices, indices);
+
+        super(mesh, Color.WHITE);
     }
 }
 
 class GroundEntity extends Entity {
 
 
-    GroundEntity(Color color, int tilesX, int tilesY) {
+    GroundEntity(Color color, int tilesX, int tilesY, float sizeX, float sizeY) {
 
         float[] vertices = new float[tilesX * tilesY * 3];
 
         for (int i = 0; i < tilesX; i++) {
             for (int j = 0; j < tilesY; j++) {
-                vertices[(i * tilesY + j) * 3] = i - tilesX / 2f;
-                vertices[(i * tilesY + j) * 3 + 2] = j - tilesY / 2f;
+                vertices[(i * tilesY + j) * 3]     = (i / (float)(tilesX - 1) - 0.5f) * sizeX;
+                vertices[(i * tilesY + j) * 3 + 2] = (j / (float)(tilesY - 1) - 0.5f) * sizeY;
             }
         }
 
@@ -109,10 +168,20 @@ class GroundEntity extends Entity {
             }
         }
 
+        float[] normals = new float[tilesX * tilesY * 3];
 
-        Mesh mesh = new Mesh(vertices, indices);
+
+        for (int i = 0; i < tilesX * tilesY; i++) {
+            normals[i * 3] = 0;
+            normals[i * 3 + 1] = 1;
+            normals[i * 3 + 2] = 0;
+        }
+
+
+        Mesh mesh = new Mesh(vertices, normals, indices);
 
         super(mesh, color);
+        hasNormals = true;
 
     }
 }
